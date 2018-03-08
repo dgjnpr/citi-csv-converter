e994731d246dcf210af6610883a00ed2c81fb0a4
David Gethings <dgethings@juniper.net>
2018-03-08 23:37:23 +0000

    got test struct working except for want field

--- main_test.go
+++ main_test.go
@@ -31,12 +31,12 @@ func TestYnabParser(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			"a test",
-			strings.NewReader(`"Account Number","Account Name","Transaction Date","Post Date","Reference Number","Transaction Detail","Billing Amount","Source Currency","Source Amount","Customer Ref","Employee Number"
+			name: "a test",
+			args: args{strings.NewReader(`"Account Number","Account Name","Transaction Date","Post Date","Reference Number","Transaction Detail","Billing Amount","Source Currency","Source Amount","Customer Ref","Employee Number"
 "XXXXXXXXXX","Foo","01/01/2018","02/01/2018","12345","my company"," -1,000","GBP"," -1,000",,"98765"
-"XXXXXXXXXX","Foo","20/02/2018","21/02/2018","23456","a shop","10.00","GBP","10.00",,"98765"`),
-			string.NewReader(`Date,Payee,Category,Memo,Outflow,Inflow\n01/01/2018,my company,Job Expense,,,"1,000"\n02/01/2018,a shop,Job Expense,,10.00,`),
-			true,
+"XXXXXXXXXX","Foo","20/02/2018","21/02/2018","23456","a shop","10.00","GBP","10.00",,"98765"`)},
+			want:    strings.NewReader(`Date,Payee,Category,Memo,Outflow,Inflow\n01/01/2018,my company,Job Expense,,,"1,000"\n02/01/2018,a shop,Job Expense,,10.00,`),
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
